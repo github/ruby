3010a5593740d4ba7b8265080390ad4b0922ef84
Alan Wu <XrXr@users.noreply.github.com>
2022-03-31 12:29:47 -0400

    Fix unused warnings in rb_yjit_insns_compiled

--- yjit/src/disasm.rs
+++ yjit/src/disasm.rs
@@ -139,9 +139,10 @@ fn disasm_iseq(iseq: IseqPtr) -> String {
 /// Primitive called in yjit.rb
 /// Produce a list of instructions compiled for an isew
 #[no_mangle]
-pub extern "C" fn rb_yjit_insns_compiled(ec: EcPtr, ruby_self: VALUE, iseqw: VALUE) -> VALUE {
+pub extern "C" fn rb_yjit_insns_compiled(_ec: EcPtr, _ruby_self: VALUE, iseqw: VALUE) -> VALUE {
     #[cfg(not(feature = "disasm"))]
     {
+        let _ = iseqw;
         return Qnil;
     }
 
