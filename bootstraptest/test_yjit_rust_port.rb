d76c72db0ba014bb2dc1a20080db3e4b9d9bdbe1
Alan Wu <XrXr@users.noreply.github.com>
2022-04-08 13:49:04 -0400

    Remove obsolete test

--- bootstraptest/test_yjit_rust_port.rb
+++ bootstraptest/test_yjit_rust_port.rb
@@ -286,37 +286,6 @@ assert_equal '[0, 1, 2, 3, 4, 5]', %q{
   expandarray_useless_splat
 }
 
-# Regression for clear_local_types()
-assert_equal '2022', %q{
-  def contrivance(hash, key)
-    # Expect this to compile to an `opt_aref`.
-    hash[key]
-
-    # The [] call above tracks that the `hash` local has a VALUE that
-    # is a heap pointer and the guard for the Kernel#itself call below
-    # doesn't check that it's a heap pointer VALUE.
-    #
-    # As you can see from the crash, the call to rb_hash_aref() can set the
-    # `hash` local, making eliding the heap object guard unsound.
-    hash.itself
-  end
-
-  # This is similar to ->(recv, mid) { send(recv, mid).local_variable_set(...) }.
-  # By composing we avoid creating new Ruby frames and so sending :binding
-  # captures the environment of the frame that does the missing key lookup.
-  # We use it to capture the environment inside of `contrivance`.
-  cap_then_set =
-    Kernel.instance_method(:send).method(:bind_call).to_proc >>
-      ->(binding) { binding.local_variable_set(:hash, 2022) }
-  special_missing = Hash.new(&cap_then_set)
-
-  # Make YJIT speculate that it's a hash and generate code
-  # that calls rb_hash_aref().
-  contrivance({}, :warmup)
-
-  contrivance(special_missing, :binding)
-}
-
 # Make sure we're correctly reading RStruct's as.ary union for embedded RStructs
 assert_equal '3,12', %q{
   pt_struct = Struct.new(:x, :y)
